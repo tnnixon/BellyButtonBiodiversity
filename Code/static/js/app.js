54ba6149788194cbdc3e73691f58c6754f558a8b
Tiera Nixon <89324254+tnnixon@users.noreply.github.com>
2022-02-19 12:15:26 -0500

    Add files via upload

---
+++ Code/static/js/app.js
@@ -0,0 +1,17 @@
+
+// function the populates the metadata
+
+
+// function that builds the bar chart
+
+
+// function that builds the bubble chart
+
+
+// function that initializes the dashboard
+
+
+// function that updates the dashboard
+
+
+// call the initialize function
